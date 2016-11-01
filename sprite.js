6eac075c26d8d5716b35195555a4a8a9317817be
Igor Knop <igorknop@gmail.com>
2016-10-31 22:20:21 -0200

    Adiciona cenários e mapas

--- sprite.js
+++ sprite.js
@@ -58,18 +58,18 @@ SpriteInMap.prototype.move = function(dt){
     this.vx = this.vx + this.ax*dt;
     this.vy = this.vy + this.ay*dt + g*dt;
 
-    if(mapa[this.my][this.mx+1]==1 && this.vx>0){
+    if(questTutorial.getCell(this.my,this.mx+1)==1 && this.vx>0){
       this.vx = Math.min(this.vx, ((this.mx+1)*32-1-this.x)/dt);
     }
-    if(mapa[this.my][this.mx-1]==1 && this.vx<0){
+    if(questTutorial.getCell(this.my,this.mx-1)==1 && this.vx<0){
       this.vx = -Math.min(Math.abs(this.vx), Math.abs((this.mx)*32+1-this.x)/dt);
     }
     this.x = (this.x + this.vx*dt);
     this.mx = Math.floor(this.x/32);
-    if(mapa[this.my-1][this.mx]==1 && this.vy<0){
+    if(questTutorial.getCell(this.my-1,this.mx)==1 && this.vy<0){
       this.vy = -Math.min(Math.abs(this.vy), (this.y-20-(this.my)*32)/dt);
     }
-    if(mapa[this.my+1][this.mx]==1){
+    if(questTutorial.getCell(this.my+1,this.mx)==1){
       this.vy = Math.min(this.vy, ((this.my+1)*32-this.y-1)/dt);
     }
 
